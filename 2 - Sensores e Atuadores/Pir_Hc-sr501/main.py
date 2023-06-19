33f03e6923bc9cd6ed48b8dcd649c6ec4cc033c6
Raposo4 <raposolizandro@gmail.com>
2023-06-19 17:11:30 -0300

    Adding Pir_Hc-sr501 code and circuit

---
+++ 2 - Sensores e Atuadores/Pir_Hc-sr501/main.py
@@ -0,0 +1,12 @@
+from machine import Pin
+import time
+
+pino = 23
+presenca = Pin(pino,Pin.IN)
+
+while True:
+    if presenca.value():
+        print('Tem movimento')
+    else:
+        print('Não tem gente')
+
